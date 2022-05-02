8bcbdc5fa77b027954bb8cf2d8038f951c2f3c57
Benjamin DENEUX <b.deneux@okp4.com>
2022-05-02 11:45:11 +0200

    refactor: make flags global

---
+++ cmd/flags.go
@@ -0,0 +1,13 @@
+package cmd
+
+const (
+	FlagChainID     = "chain-id"
+	FlagMnemonic    = "mnemonic"
+	FlagGrpcAddress = "grpc-address"
+	FlagDenom       = "denom"
+	FlagPrefix      = "prefix"
+	FlagFeeAmount   = "fee-amount"
+	FlagAmountSend  = "amount-send"
+	FlagMemo        = "memo"
+	FlagGasLimit    = "gas-limit"
+)
