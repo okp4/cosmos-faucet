eb72871fea6cd26db3497798bd97ebf9677ffc4e
Benjamin DENEUX <b.deneux@okp4.com>
2022-05-02 14:04:45 +0200

    feat: allow changing rest server port address

--- cmd/start.go
+++ cmd/start.go
@@ -1,28 +1,36 @@
 package cmd
 
 import (
-	"github.com/gorilla/mux"
-	"github.com/spf13/cobra"
-	"net/http"
-	"okp4/cosmos-faucet/rest"
+    "github.com/gorilla/mux"
+    "github.com/spf13/cobra"
+    "net/http"
+    "okp4/cosmos-faucet/rest"
+)
+
+const (
+    FlagAddress = "address"
 )
 
 // NewStartCommand returns a CLI command to start the REST api allowing to send tokens.
 func NewStartCommand() *cobra.Command {
-	startCmd := &cobra.Command{
-		Use:   "start",
-		Short: "Start the REST api",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			router := mux.NewRouter().StrictSlash(true)
-			router.HandleFunc("/send/{address}", rest.NewSendRequestHandlerFn(config)).Methods("GET")
+    var addr string
 
-			return http.ListenAndServe(":10000", router)
-		},
-	}
+    startCmd := &cobra.Command{
+        Use:   "start",
+        Short: "Start the REST api",
+        RunE: func(cmd *cobra.Command, args []string) error {
+            router := mux.NewRouter().StrictSlash(true)
+            router.HandleFunc("/send/{address}", rest.NewSendRequestHandlerFn(config)).Methods("GET")
 
-	return startCmd
+            return http.ListenAndServe(addr, router)
+        },
+    }
+
+    startCmd.Flags().StringVar(&addr, FlagAddress, ":8080", "rest api address")
+
+    return startCmd
 }
 
 func init() {
-	rootCmd.AddCommand(NewStartCommand())
+    rootCmd.AddCommand(NewStartCommand())
 }
