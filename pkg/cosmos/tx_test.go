cc04e76413f11dba248b7bcdcaea4b6e0fedbbb8
Benjamin DENEUX <b.deneux@okp4.com>
2022-05-05 10:23:01 +0200

    test: add unsigned tx build tests

--- pkg/cosmos/tx_test.go
+++ pkg/cosmos/tx_test.go
@@ -1,9 +1,52 @@
 package cosmos
 
 import (
-	. "github.com/smartystreets/goconvey/convey"
 	"testing"
+
+	"okp4/cosmos-faucet/pkg"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/simapp"
+	"github.com/cosmos/cosmos-sdk/types"
+	. "github.com/smartystreets/goconvey/convey"
 )
+
+func newTxConfig() client.TxConfig {
+	enc := simapp.MakeTestEncodingConfig()
+	return enc.TxConfig
+}
+
+func TestBuildUnsignedTx(t *testing.T) {
+	Convey("Given a config file and address", t, func() {
+		config := pkg.Config{
+			Denom:      "know",
+			Prefix:     "okp4",
+			FeeAmount:  20,
+			AmountSend: 10,
+			Memo:       "memo",
+			GasLimit:   2000,
+		}
+		fromAddr := types.AccAddress("okp4AAAAA")
+		toAddr := types.AccAddress("okp4BBB")
+
+		Convey("When build unsigned transaction", func() {
+			unsignedTx, err := BuildUnsignedTx(config, newTxConfig(), fromAddr, toAddr)
+
+			Convey("Unsigned transaction should be successfully build", func() {
+				So(err, ShouldBeNil)
+				So(unsignedTx, ShouldNotBeNil)
+			})
+
+			Convey("Transaction should be correctly configured", func() {
+				So(unsignedTx.GetTx().GetGas(), ShouldEqual, config.GasLimit)
+				So(unsignedTx.GetTx().GetFee().String(),
+					ShouldEqual,
+					types.NewCoins(types.NewInt64Coin(config.Denom, config.FeeAmount)).String())
+				So(unsignedTx.GetTx().GetMemo(), ShouldEqual, config.Memo)
+			})
+		})
+	})
+}
 
 func TestGeneratePrivateKey(t *testing.T) {
 	Convey("Given a well formed mnemonic", t, func() {
