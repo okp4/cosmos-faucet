ae4c014f597782fb31d0ee55108fbcf928afb67c
Ingvaar <19124383+ingvaar@users.noreply.github.com>
2022-05-05 13:19:37 +0200

    fix: removed unused param in newSendRequestHandler

--- pkg/server/router.go
+++ pkg/server/router.go
@@ -1,16 +1,14 @@
 package server
 
 import (
-	"context"
-
-	"okp4/cosmos-faucet/pkg/client"
+    "okp4/cosmos-faucet/pkg/client"
 )
 
 func (s *httpServer) createRoutes(faucet *client.Faucet) {
 	s.router.Use(prometheusMiddleware)
 	s.router.Path("/").
 		Queries("address", "{address}").
-		HandlerFunc(newSendRequestHandlerFn(context.Background(), faucet)).
+		HandlerFunc(newSendRequestHandlerFn(faucet)).
 		Methods("GET")
 	s.router.Path("/health").
 		HandlerFunc(newHealthRequestHandlerFunc()).
--- pkg/server/send.go
+++ pkg/server/send.go
@@ -1,7 +1,6 @@
 package server
 
 import (
-	"context"
 	"net/http"
 
 	"okp4/cosmos-faucet/pkg/client"
@@ -11,7 +10,7 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-func newSendRequestHandlerFn(ctx context.Context, faucet *client.Faucet) http.HandlerFunc {
+func newSendRequestHandlerFn(faucet *client.Faucet) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
 		bech32Addr := vars["address"]
