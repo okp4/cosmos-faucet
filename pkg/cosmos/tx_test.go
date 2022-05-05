228daf741161d53b54f095999fedc2bceb63486a
Benjamin DENEUX <b.deneux@okp4.com>
2022-05-05 11:27:00 +0200

    test: add sign transaction test

--- pkg/cosmos/tx_test.go
+++ pkg/cosmos/tx_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/simapp"
 	"github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/auth/signing"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
@@ -43,6 +44,48 @@ func TestBuildUnsignedTx(t *testing.T) {
 					ShouldEqual,
 					types.NewCoins(types.NewInt64Coin(config.Denom, config.FeeAmount)).String())
 				So(unsignedTx.GetTx().GetMemo(), ShouldEqual, config.Memo)
+			})
+		})
+	})
+}
+
+func TestSignTx(t *testing.T) {
+	Convey("Given a private key, account and message", t, func() {
+		mnemonic := "nasty random alter chronic become keen stadium test chaos fashion during claim rug thing trade swap bleak shuffle bronze gun tobacco length aim hazard"
+		privKey, _ := GeneratePrivateKey(mnemonic)
+		config := pkg.Config{
+			Denom:      "know",
+			Prefix:     "okp4",
+			FeeAmount:  20,
+			AmountSend: 10,
+			Memo:       "memo",
+			GasLimit:   2000,
+		}
+		fromAddr := types.AccAddress("okp4AAAAA")
+		toAddr := types.AccAddress("okp4BBB")
+		signerData := signing.SignerData{
+			ChainID:       "chain-id",
+			AccountNumber: 10,
+			Sequence:      54,
+		}
+		tx, _ := BuildUnsignedTx(config, newTxConfig(), fromAddr, toAddr)
+
+		Convey("When sign transaction", func() {
+			err := SignTx(privKey, signerData, newTxConfig(), tx)
+
+			Convey("Sign transaction should be successful", func() {
+				So(err, ShouldBeNil)
+				signatures, err := tx.GetTx().GetSignaturesV2()
+				So(signatures, ShouldNotBeEmpty)
+				So(err, ShouldBeNil)
+
+				Convey("Signature ppublic key should be the same as the signer", func() {
+					So(signatures[0].PubKey.String(), ShouldEqual, privKey.PubKey().String())
+				})
+
+				Convey("Account sequence and number should be correctly set", func() {
+					So(signatures[0].Sequence, ShouldEqual, signerData.Sequence)
+				})
 			})
 		})
 	})
