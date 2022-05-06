6f92e73a3fade89955ed79dea9b9dab254836586
Ingvaar <19124383+ingvaar@users.noreply.github.com>
2022-05-06 18:05:26 +0200

    feat: handle health check response error if write fails

--- pkg/server/health.go
+++ pkg/server/health.go
@@ -1,10 +1,17 @@
 package server
 
-import "net/http"
+import (
+    "fmt"
+    "net/http"
+
+    "github.com/rs/zerolog/log"
+)
 
 func newHealthRequestHandlerFunc() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		_, _ = w.Write([]byte(""))
-	}
+    return func(w http.ResponseWriter, r *http.Request) {
+        w.WriteHeader(http.StatusOK)
+        if _, err := w.Write([]byte("")); err != nil {
+            log.Error().Msg(fmt.Sprintf("Error while sending health response: %v", err))
+        }
+    }
 }
