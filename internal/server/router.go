ea5674e40d853ab68861f240cacbf105f2d1c478
Benjamin DENEUX <b.deneux@okp4.com>
2022-05-10 10:11:05 +0200

    fix: add method on graphql route

--- internal/server/router.go
+++ internal/server/router.go
@@ -4,7 +4,6 @@ import (
 	"okp4/cosmos-faucet/graph"
 	"okp4/cosmos-faucet/graph/generated"
 	"okp4/cosmos-faucet/internal/server/handlers"
-	"okp4/cosmos-faucet/pkg/server"
 
 	graphql "github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
@@ -13,14 +12,12 @@ import (
 func (s *httpServer) createRoutes(config Config) {
 	s.router.Use(handlers.PrometheusMiddleware)
 	s.router.Path("/").
-		HandlerFunc(playground.Handler("GraphQL playground", "/query"))
+		HandlerFunc(playground.Handler("GraphQL playground", "/query")).
+		Methods("GET")
 	s.router.Path("/query").
 		Handler(graphql.NewDefaultServer(generated.NewExecutableSchema(generated.
-			Config{Resolvers: &graph.Resolver{Faucet: faucet}})))
-	s.router.Path("/").
-		Queries("address", "{address}").
-		HandlerFunc(server.NewSendRequestHandlerFn(config.Faucet)).
-		Methods("GET")
+			Config{Resolvers: &graph.Resolver{Faucet: config.Faucet}}))).
+		Methods("GET", "POST", "OPTIONS")
 	if config.EnableHealth {
 		s.router.Path("/health").
 			HandlerFunc(handlers.NewHealthRequestHandlerFunc()).
