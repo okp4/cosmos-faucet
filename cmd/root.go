2568c7629a8dc27d055f8de80550f80225d04f23
Arnaud Mimart <33665250+amimart@users.noreply.github.com>
2022-08-18 19:04:17 +0200

    refactor(cmd): use seperate vars to parse cli args

--- cmd/root.go
+++ cmd/root.go
@@ -1,19 +1,20 @@
 package cmd
 
 import (
+	"crypto/tls"
 	"errors"
 	"fmt"
 	"os"
 	"strings"
 	"time"
 
-	"okp4/cosmos-faucet/pkg"
-
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 const (
@@ -21,7 +22,20 @@ const (
 	envPrefix             = "FAUCET"
 )
 
-var config pkg.Config
+var (
+	mnemonic      string
+	chainID       string
+	grpcAddress   string
+	denom         string
+	prefix        string
+	feeAmount     int64
+	amountSend    int64
+	memo          string
+	gasLimit      uint64
+	noTLS         bool
+	tlsSkipVerify bool
+	txTimeout     time.Duration
+)
 
 // NewRootCommand returns the root CLI command with persistent flag handling.
 var rootCmd = &cobra.Command{
@@ -37,21 +51,21 @@ var rootCmd = &cobra.Command{
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	rootCmd.PersistentFlags().StringVar(&config.Mnemonic, FlagMnemonic, "", "")
-	rootCmd.PersistentFlags().StringVar(&config.ChainID, FlagChainID, "localnet-okp4-1", "The network chain ID")
-	rootCmd.PersistentFlags().StringVar(&config.GrpcAddress, FlagGrpcAddress, "127.0.0.1:9090", "The grpc okp4 server url")
-	rootCmd.PersistentFlags().StringVar(&config.Denom, FlagDenom, "know", "Token denom")
-	rootCmd.PersistentFlags().StringVar(&config.Prefix, FlagPrefix, "okp4", "Address prefix")
-	rootCmd.PersistentFlags().Int64Var(&config.FeeAmount, FlagFeeAmount, 0, "Fee amount")
-	rootCmd.PersistentFlags().Int64Var(&config.AmountSend, FlagAmountSend, 1, "Amount send value")
-	rootCmd.PersistentFlags().StringVar(&config.Memo, FlagMemo, "Sent by økp4 faucet", "The memo description")
-	rootCmd.PersistentFlags().Uint64Var(&config.GasLimit, FlagGasLimit, 200000, "Gas limit")
-	rootCmd.PersistentFlags().BoolVar(&config.NoTLS, FlagNoTLS, false, "No encryption with the GRPC endpoint")
-	rootCmd.PersistentFlags().BoolVar(&config.TLSSkipVerify,
+	rootCmd.PersistentFlags().StringVar(&mnemonic, FlagMnemonic, "", "")
+	rootCmd.PersistentFlags().StringVar(&chainID, FlagChainID, "localnet-okp4-1", "The network chain ID")
+	rootCmd.PersistentFlags().StringVar(&grpcAddress, FlagGrpcAddress, "127.0.0.1:9090", "The grpc okp4 server url")
+	rootCmd.PersistentFlags().StringVar(&denom, FlagDenom, "know", "Token denom")
+	rootCmd.PersistentFlags().StringVar(&prefix, FlagPrefix, "okp4", "Address prefix")
+	rootCmd.PersistentFlags().Int64Var(&feeAmount, FlagFeeAmount, 0, "Fee amount")
+	rootCmd.PersistentFlags().Int64Var(&amountSend, FlagAmountSend, 1, "Amount send value")
+	rootCmd.PersistentFlags().StringVar(&memo, FlagMemo, "Sent by økp4 faucet", "The memo description")
+	rootCmd.PersistentFlags().Uint64Var(&gasLimit, FlagGasLimit, 200000, "Gas limit")
+	rootCmd.PersistentFlags().BoolVar(&noTLS, FlagNoTLS, false, "No encryption with the GRPC endpoint")
+	rootCmd.PersistentFlags().BoolVar(&tlsSkipVerify,
 		FlagTLSSkipVerify,
 		false,
 		"Encryption with the GRPC endpoint but skip certificates verification")
-	rootCmd.PersistentFlags().DurationVar(&config.TxTimeout, FlagTxTimeout, 5*time.Second, "Transaction timeout")
+	rootCmd.PersistentFlags().DurationVar(&txTimeout, FlagTxTimeout, 5*time.Second, "Transaction timeout")
 
 	err := rootCmd.Execute()
 	if err != nil {
@@ -84,4 +98,15 @@ func ReadPersistentFlags(cmd *cobra.Command) error {
 	})
 
 	return nil
+}
+
+func getTransportCredentials() credentials.TransportCredentials {
+	switch {
+	case noTLS:
+		return insecure.NewCredentials()
+	case tlsSkipVerify:
+		return credentials.NewTLS(&tls.Config{InsecureSkipVerify: true}) // #nosec G402 : skip lint since it's an optional flag
+	default:
+		return credentials.NewTLS(&tls.Config{MinVersion: tls.VersionTLS12})
+	}
 }
